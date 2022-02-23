cb307f1e5ba561ec92cea8e95302e2a739527caa
zakhro-yusupova <yu.zakhro01@gmail.com>
2022-02-23 18:39:09 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var UZS =10839;
+var RUBL = 7990;
+var EURO = 0.88;
+
+var elForm = document.querySelector(".site-form");
+var elFormInput = document.querySelector(".site-form__input");
+var elFormSelect = document.querySelector(".site-form__select");
+var elFormResult = document.querySelector(".site-form__result");
+
+elForm.addEventListener("submit" , function(e){
+
+  e.preventDefault();
+
+  var elFormInputVal = Number(elFormInput.value);
+  var elFormSelectVal = Number(elFormSelect.value);
+
+  elFormResult.textContent = (elFormInputVal * elFormSelectVal);
+  console.log(elFormInputVal * elFormSelectVal);
+});
